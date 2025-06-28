abe5ae660879eb1f4000a6fa9cd6b2d5fa961b8c
thaijaso <thaijaso@gmail.com>
2025-06-28 15:37:27 -0700

    add animation event logs

--- Assets/Scripts/Enemy/States/AggroState.cs
+++ Assets/Scripts/Enemy/States/AggroState.cs
@@ -19,11 +19,13 @@ public class AggroState : EnemyState
     public override void Enter()
     {
         base.Enter();
+        Debug.Log("AggroState: Entering Aggro state - yell animation should start");
         animationManager.SetIsAggro(true); // Set aggro state in animation manager
     }
 
     public void OnYellFinished()
     {
+        Debug.Log("AggroState: Yell animation finished - enabling AI movement");
         enemy.AIDestinationSetter.enabled = true;
     }
 }
